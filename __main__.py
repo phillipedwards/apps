1777bfeabc72063049dc9dc83c26e61e63a16182
Phillip Edwards <phillip@pulumi.com>
2023-08-27 07:16:28 -0700

    init

---
+++ __main__.py
@@ -0,0 +1,57 @@
+"""An AWS Python Pulumi program"""
+
+import pulumi
+import pulumi_kubernetes as k8s
+
+stack_ref = pulumi.StackReference("team-ce/cluster/dev")
+kubeconfig = stack_ref.require_output("kubeconfig")
+provider = k8s.Provider(
+    "provider",
+    kubeconfig=kubeconfig
+)
+
+ns = k8s.core.v1.Namespace(
+    "ts-ns",
+    metadata=k8s.meta.v1.ObjectMetaArgs(
+        name="nginx"
+    ),
+    opts=pulumi.ResourceOptions(
+        provider=provider
+    )
+)
+
+image_tag = "nginx:1.25"
+app = { "app": "nginx" }
+k8s.apps.v1.Deployment(
+    "nginx",
+    metadata=k8s.meta.v1.ObjectMetaArgs(
+        namespace=ns.metadata.name,
+        name="nginx",
+    ),
+    spec=k8s.apps.v1.DeploymentSpecArgs(
+        selector=k8s.meta.v1.LabelSelectorArgs(
+            match_labels=app,
+        ),
+        replicas=2,
+        template=k8s.core.v1.PodTemplateSpecArgs(
+            metadata=k8s.meta.v1.ObjectMetaArgs(
+                labels=app,
+            ),
+            spec=k8s.core.v1.PodSpecArgs(
+                containers=[k8s.core.v1.ContainerArgs(
+                    name="nginx",
+                    image=image_tag,
+                    ports=[k8s.core.v1.ContainerPortArgs(
+                        container_port=80
+                    )],
+                )]
+            )
+        )
+    ),
+    opts=pulumi.ResourceOptions(
+        provider=provider,
+        ignore_changes=["spec.replicas"]
+    )
+)
+
+pulumi.export("kubeconfig", kubeconfig)
